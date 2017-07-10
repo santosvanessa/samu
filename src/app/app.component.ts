af23856dda76563db0391e9a8a105ccb87f392dc
vanessa santos <vanessa.explore1d@gmail.com>
2017-07-10 11:14:02 -0300

    prova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,17 @@ import {SamuService} from './services/samu.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UFService, SamuService]
 })
 export class AppComponent implements OnInit {
     title = 'app';
     ufs : UF[];
     dados_da_samu : Dados[];
+    Minha_uf: UF;
+    munatendidos: Dados[] = [];
+    meuid = 52;
+    media: number;
 
     constructor(private ufService: UFService, private samuService: SamuService)
     { }
@@ -22,5 +27,25 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
+        this.Minha_uf = this.defineUF();
+        this.media = this.calcularMunicipios();
     }
-}
+
+    defineUF(): UF {
+      for(let uf of this.ufs){
+        if(uf.id == this.meuid) return uf;
+      }
+    }
+
+    calcularMunicipios(): number {
+      var qnt = 0;
+      var total = 0;
+      for(let mun of this.dados_da_samu){
+        if(mun.uf_id == this.meuid){
+          qnt++;
+          total+=mun.valor;
+          this.munatendidos.push(mun);
+        }
+      }
+return Math.round(total/qnt);
+}}
